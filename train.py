4bed59f226d7ce6e0e01c9ce045c19c22b39e1c4
阿铭 <item_aming@163.com>
2024-06-26 04:07:07 +0800

    更新train

--- train.py
+++ train.py
@@ -23,9 +23,6 @@ model.train(data=data, epochs=1, batch=4)
 
 # Validate the model
 metrics = model.val()  # no arguments needed, dataset and settings remembered
-print(metrics.box.map)  # map50-95
-print(metrics.box.map50)  # map50
-print(metrics.box.map75)  # map75
-print(metrics.box.maps)  # a list contains map50-95 of each category
+print(f"source: {metrics.box.map}")  # map50-95
 
 
